a2f91f159ee3c30788278ac40d4fbfb428015bd5
B00150644 <B00150644@mytudublin.ie>
2023-10-31 16:28:15 +0000

    ui

--- GameProject/Assets/Scripts/Timer.cs
+++ GameProject/Assets/Scripts/Timer.cs
@@ -9,20 +9,19 @@ public class Timer : MonoBehaviour
     public Text timerText;
     static float timer;
 
+    public Image[] stars; // Attach your star images in the inspector
     public Text lapText;
 
     // Start is called before the first frame update
     void Start()
     {
-         
-
-
     }
 
-    public void RestartGame() 
+    public void RestartGame()
     {
-        SceneManager.LoadScene("SampleScene");   
+        SceneManager.LoadScene("SampleScene");
     }
+
     // Update is called once per frame
     void Update()
     {
@@ -35,15 +34,42 @@ public class Timer : MonoBehaviour
 
         timerText.text = time;
 
-        if (Input.GetKeyDown("9")) 
+        if (Input.GetKeyDown("9"))
         {
             RestartGame();
         }
 
+        UpdateStars(); // Call the function to update stars
     }
+
     private void OnTriggerEnter(Collider other)
     {
         lapText.text = "Lap: " + timerText.text;
         timer = 0.0f;
     }
+
+    private void UpdateStars()
+    {
+        if (timer < 180) // Less than 3 minutes
+        {
+            SetStars(3); // Show 3 stars
+        }
+        else if (timer < 240) // Less than 4 minutes
+        {
+            SetStars(2); // Show 2 stars
+        }
+        else
+        {
+            SetStars(1); // Show 1 star
+        }
+    }
+
+    private void SetStars(int numStars)
+    {
+        for (int i = 0; i < stars.Length; i++)
+        {
+            stars[i].enabled = i < numStars;
+        }
+    }
 }
+
